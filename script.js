7cc5d68d8544e22cd806f89582b77033de2a7daf
bobsmith947 <bobsmith947@users.noreply.github.com>
2017-11-01 13:19:54 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+//set link class according to href
+function setLinks() {
+  var elems = document.links;
+  for (var i = 0; i < elems.length; i++) {
+    if (elems[i].href.includes(document.domain)) elems[i].className = "int";
+    else elems[i].className = "ext";
+  }
+  elems = document.getElementsByClassName("ext");
+  for (var i = 0; i < elems.length; i++) elems[i].target = "_blank";
+  return null;
+}
+//clickable boxes
+var prev = null;
+document.body.onclick = ev => {
+  var tar = (ev.target) ? ev.target : ev.srcElement;
+  if (prev !== null) {
+    if (prev.parentElement.className === "box") resetBox(prev.parentElement);
+    else if (prev.className === "box") resetBox(prev);
+  }
+  if (tar.parentElement.className === "box") selectBox(tar.parentElement);
+  else if (tar.className === "box") selectBox(tar);
+  prev = tar;
+}
+function selectBox(elem) {
+  elem.style.overflow = "auto";
+  elem.style.border = "3px solid gray";
+  return elem;
+}
+function resetBox(elem) {
+  elem.style.overflow = "hidden";
+  elem.style.border = "3px dashed gray";
+  return elem;
+}
+//konami code easter egg
+var keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65], pos = 0;
+document.body.onkeydown = ev => {
+  var key = ev.keyCode;
+  if (key === keys[pos]) {
+    pos++;
+    if(pos >= keys.length) {
+      pos = 0;
+      document.getElementById("top").srcset = "tiger.png 1280w";
+    }
+  } else pos = 0;
+}
