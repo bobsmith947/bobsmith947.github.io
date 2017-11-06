f0c58ee6d74370dccf13de33cb0e673d7180c085
bobsmith947 <bobsmith947@users.noreply.github.com>
2017-11-06 14:58:50 -0700

    improve js

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
+"use strict";
 function setLinks() {
-  var elems = document.links;
-  for (var i = 0; i < elems.length; i++) {
+  var elems = document.links, len = elems.length, i = undefined;
+  for (i = 0; i < len; i++) {
     if (elems[i].href.includes(document.domain)) elems[i].className = "int";
     else elems[i].className = "ext";
   }
   elems = document.getElementsByClassName("ext");
-  for (var i = 0; i < elems.length; i++) elems[i].target = "_blank";
-  return null;
+  for (i = 0; i < len; i++) elems[i].target = "_blank";
+  return undefined;
 }
 function boxAdapt() {
   var tip = document.getElementById("boxTip"), str = "each box to view the full list.";
   try {
-  if (screen.width <= 768) tip.innerHTML = "Tap " + str;
-  else tip.innerHTML = "Click " + str;
+    if (screen.width <= 768) tip.innerHTML = "Tap " + str;
+    else tip.innerHTML = "Click " + str;
 } finally {
-  return null;
+    return undefined;
 }
 }
 document.body.onload = setLinks();
 document.body.onload = boxAdapt();
 //clickable boxes
-var prev = null;
+var prev = undefined;
 document.body.onclick = ev => {
   var tar = ev.target;
-  if (prev !== null) {
+  if (prev !== undefined) {
     if (prev.parentElement.className === "box") resetBox(prev.parentElement);
     else if (prev.className === "box") resetBox(prev);
   }
@@ -34,12 +35,12 @@ document.body.onclick = ev => {
 function selectBox(elem) {
   elem.style.overflow = "auto";
   elem.style.border = "3px solid gray";
-  return null;
+  return undefined;
 }
 function resetBox(elem) {
   elem.style.overflow = "hidden";
   elem.style.border = "3px dashed gray";
-  return null;
+  return undefined;
 }
 //konami code easter egg
 var keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65], pos = 0;
