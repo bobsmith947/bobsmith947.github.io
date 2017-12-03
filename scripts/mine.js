5dad70f5d81b33868cf4372aac2cfd1687fc5bb7
bobsmith947 <bobsmith947@users.noreply.github.com>
2017-12-03 14:15:42 -0700

    update miner

--- scripts/mine.js
+++ scripts/mine.js
@@ -1,17 +1,17 @@
 "use strict";
-var miner = document.getElementById("mine");
-miner.addEventListener("error", fail);
-miner.addEventListener("load", success);
-function fail(ev) {
+document.getElementById("mine").onerror = () => {
   var node = document.createElement("h4");
   var text = document.createTextNode("Please allow scripts from authedmine.com.");
   node.appendChild(text);
   document.getElementById("notify").appendChild(node);
 }
-function success(ev) {
-  document.querySelector(".coinhive-miner").style.height = "310px";
+document.getElementById("mine").onload = () => {
   var node = document.createElement("h4");
   var text = document.createTextNode("It's not a good idea to increase threads past what it initially is.");
   node.appendChild(text);
   document.getElementById("notify").appendChild(node);
+  document.querySelector(".coinhive-miner").style.height = "310px";
+  window.addEventListener("beforeunload", ev => {
+    ev.returnValue = "The miner may be running. Are you sure want to leave this page?";
+  })
 }
