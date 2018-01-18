f694dc2412006e6178ebbdaf792191085a3486b2
Lucas Kitaev <lkitaev@cherrycreekschools.org>
2018-01-18 12:13:36 -0700

    Update script.js (#9)

--- scripts/browserCheck.js
+++ scripts/browserCheck.js
@@ -1,18 +1,27 @@
-//alert user of old browser
-/*window.onerror = function() {
-  window.alert("Your browser is outdated! Please update otherwise some features will not work!");
-};*/
-
+//window.onerror = window.alert("Something went wrong, you may want to look into upgrading your browser.");
 if (detectIE()) {
-  window.alert("Your browser has been detected to be Internet Explorer. The layout may appear broken. It is recommended to switch to a newer browser.")
+  window.alert("Your browser has been detected to be Internet Explorer. The layout may appear broken. It is recommended to switch to a newer browser.");
   //window.alert("Please don't use Internet Explorer! The site will not work!");
   //window.open("https://www.google.com/chrome/browser/desktop/index.html", "_self");
 }
 
+// Taken from https://www.sitepoint.com/profiling-page-loads-with-the-navigation-timing-api/
+if (window.performance && window.performance.timing) {
+    window.addEventListener("load", function() {
+        var timing = window.performance.timing;
+        var user = timing.loadEventEnd - timing.navigationStart;
+        var dns = timing.domainLookupEnd - timing.domainLookupStart;
+        var conn = timing.connectEnd - timing.connectStart;
+        var req = timing.responseEnd - timing.requestStart;
+        var fetch = timing.responseEnd - timing.fetchStart;
+        console.log(user + " " + dns + " " + conn + " " + req + " " + fetch);
+});
+}
+
+// Taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 if (!String.prototype.includes) {
   String.prototype.includes = function(search, start) {
-    'use strict';
-    if (typeof start !== 'number') {
+    if (typeof start !== "number") {
       start = 0;
     }
     
@@ -31,16 +40,16 @@ function detectIE() {
   // Test values; Uncomment to check result …
 
   // IE 10
-  // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+  //ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
 
   // IE 11
-  // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+  //ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
 
   // Edge 12 (Spartan)
-  // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+  //ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
 
   // Edge 13
-  // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+  //ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
 
   var msie = ua.indexOf('MSIE ');
   if (msie > 0) {
@@ -61,6 +70,6 @@ function detectIE() {
     //return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
     return false;
   }
-  // other browser
+  // Other browser
   return false;
 }
